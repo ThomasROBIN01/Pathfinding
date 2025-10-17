ac4a436b10119ef9d7e97d7609d7869c8f2962c7
Thomas <thomas-robin@hotmail.fr>
2025-10-17 10:47:21 +0800

    Automatic Search
    - SpaceBar to launch the automatic neighbour search
    - Colouring the node to visually see the path to find the target node

--- Assets/Scripts/BFS Script/BreadthFirstSearch.cs
+++ Assets/Scripts/BFS Script/BreadthFirstSearch.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class BreadthFirstSearch : MonoBehaviour
 {
@@ -16,16 +17,56 @@ public class BreadthFirstSearch : MonoBehaviour
     // List of nodes in queue
     [SerializeField] List<Node> nodeQueue = new List<Node>();
 
+    // create new instance of InputSystem_Actions
+    // InputSystem_Actions can be found in the assets roots folder in Unity: we added a new one in Action Maps / UI / Submit 
+    private InputSystem_Actions playerActions;
+
+    [SerializeField] Material redMat, blueMat, greenMat, yellowMat;
+
+
+    private void Awake()
+    {
+        // Initialize the InputSystem_Action
+        playerActions = new InputSystem_Actions();
+    }
+
+
+    private void OnEnable()
+    {
+        // make sure that the playerActions 'Player' action map is enabled
+        playerActions.UI.Enable();
+    }
+
+    private void Update()
+    {
+        // check if the 'Submit' input was pressed this frame
+        if (playerActions.UI.Submit.WasPressedThisFrame())
+        {
+            BreadthFirstAlgorithm();
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        BreadthFirstAlgorithm();
+        // BreadthFirstAlgorithm();
     }
 
 
     private void BreadthFirstAlgorithm()
     {
+
+        // To visualisy represent the nodes affected, we will attribute some colours to the nodes:
+
+        // the start node in blue:
+        MeshRenderer startNodeMeshRenderer = startNode.GetComponent<MeshRenderer>();
+        startNodeMeshRenderer.material = blueMat;
+
+        // the target node in green:
+        MeshRenderer targetNodeMeshRenderer = targetNode.GetComponent<MeshRenderer>();
+        targetNodeMeshRenderer.material = greenMat;
+
+
         // add 'start node' to nodeQueue
 
         nodeQueue.Add(startNode);
@@ -66,6 +107,10 @@ public class BreadthFirstSearch : MonoBehaviour
                 }
                 else
                 {
+                    // turn a visited node in red once we confirm it isn't the target
+                    MeshRenderer visitedNodeRenderer = activeNode.GetComponent<MeshRenderer>();
+                    visitedNodeRenderer.material = redMat;
+
                     // if no: move 'active node' out of queue and into 'visitedNodes', then continue
                     nodeQueue.Remove(activeNode);
                     visitedNodes.Add(activeNode);
--- Assets/Scripts/BFS Script/Node.cs
+++ Assets/Scripts/BFS Script/Node.cs
@@ -5,5 +5,62 @@ public class Node : MonoBehaviour
 {
     public List<Node> neighbours = new List<Node>();
 
+    private void Start()
+    {
+        FindNeighbours();
+    }
 
+    private void FindNeighbours()
+    {
+        // Find out which object are adjacent / nearby this one
+        Collider[] objectsNearMe = Physics.OverlapBox(transform.position, transform.localScale / 1.8f);
+
+        // assign the north neighbour
+        foreach (Collider obj in objectsNearMe)
+        {
+            if (obj.TryGetComponent(out Node node))
+            {
+                if (node.transform.position.z == transform.position.z + 1 && node.transform.position.x == transform.position.x)
+                {
+                    neighbours.Add(node);
+                }
+            }
+        }
+
+        // assign the east neighbour
+        foreach (Collider obj in objectsNearMe)
+        {
+            if (obj.TryGetComponent(out Node node))
+            {
+                if (node.transform.position.x == transform.position.x + 1 && node.transform.position.z == transform.position.z)
+                {
+                    neighbours.Add(node);
+                }
+            }
+        }
+
+        // assign the south neighbour
+        foreach (Collider obj in objectsNearMe)
+        {
+            if (obj.TryGetComponent(out Node node))
+            {
+                if (node.transform.position.z == transform.position.z - 1 && node.transform.position.x == transform.position.x)
+                {
+                    neighbours.Add(node);
+                }
+            }
+        }
+
+        // assign the west neighbour
+        foreach (Collider obj in objectsNearMe)
+        {
+            if (obj.TryGetComponent(out Node node))
+            {
+                if (node.transform.position.x == transform.position.x - 1 && node.transform.position.z == transform.position.z)
+                {
+                    neighbours.Add(node);
+                }
+            }
+        }
+    }
 }
